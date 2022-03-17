842b5ee070e840c1a290cb3b8bfc29ff9abc850d
gregory arcentales <gregoarcenta@gmail.com>
2022-03-17 01:47:03 -0500

    Se agrega formularios para checkbox, radios y switches con validaciones por template

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.css']
 })
-export class SwitchesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SwitchesComponent {
+  persona = {
+    genero: 'M',
+    notificaciones: false
   }
+
+  terminosYCondiciones = false;
 
 }
